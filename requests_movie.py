802c25624f4e8ff453dd265b926027fe9abf350e
Haydee Walrus <haydeewa@mi-b4.msk.21-school.ru>
2023-12-19 16:24:28 +0300

    migration to aiogram 3.2.0. Its work!

--- requests_movie.py
+++ requests_movie.py
@@ -1,68 +1,66 @@
+import asyncio
 import requests
-import json
-import urllib
-from aiogram import Bot, Dispatcher, executor, types
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+import aiohttp
 
 API_TOKEN = '6894979902:AAEbC0-cA2Q-I29SZ5h53mGBmQwGB9ER7Ok'
 bot = Bot(token=API_TOKEN)
-dp = Dispatcher(bot)
+dp = Dispatcher()
 
 token_kinopoisk = "2MPR2W5-BKP4DAB-KVCT0FC-6TXFK5Q"
-url = "https://api.kinopoisk.dev/v1.4/movie/random?rating.imdb=7-10&lists=top250"  # Замени адрес сайта и путь до эндпоинта на свои
-
+url = "https://api.kinopoisk.dev/v1.4/movie/random?rating.imdb=7-10&lists=top250"
 headers = {
     "X-API-KEY": f"{token_kinopoisk}"
 }
 
 
-@dp.message_handler(commands=['start'])
+@dp.message(Command('start'))
 async def send_welcome(message: types.Message):
     await message.reply("Hello!")
 
 
-@dp.message_handler()
+@dp.message()
 async def start_request(message: types.Message):
-    response_list = get_movie()
+    response_list = await get_movie()
+
     poster_url = response_list[0]
+
+    response_list.pop(0)
     response_str = "\n".join(map(str, response_list))
 
-    urllib.request.urlretrieve(f"{poster_url}", "image.jpg")
+    await message.answer_photo(poster_url, response_str)
 
-    with open("image.jpg", "rb") as photo:
-        await message.answer_photo(photo, response_str)
 
-    os.remove("image.jpg")
+async def get_movie():
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, headers=headers, ssl=False) as resp:
+            if resp.status == 200:
+                data = await resp.json()
 
-def get_movie():
-    response = requests.get(url, headers=headers)
+                img = data["poster"]["url"]
+                name = data["names"][0]["name"]
+                rating = data["rating"]["imdb"]
+                year = data["year"]
+                description = data["description"]
 
-    if response.status_code == 200:
-        data = response.json()
-        # Здесь можешь обрабатывать полученные данные
-        
-        print(json.dumps(data, indent=4))
-        img = data["poster"]["url"]
-        name = data["names"][0]["name"]
-        rating = data["rating"]["imdb"]
-        year = data["year"]
-        description = data["description"]
-        # trailers = json.dumps(data["videos"]["trailers"], indent=4)
-        trailers_urls = []
-        trailers = data["videos"]["trailers"]
-        for trailer in trailers:
-            trailers_urls.append(trailer["url"])
+                trailers_urls = [trailer["url"] for trailer in data["videos"]["trailers"]]
 
-        print(name)
-        print(rating)
-        print(year)
-        print(description)
-        print(*trailers_urls, sep="\n")
+                print(name)
+                print(rating)
+                print(year)
+                print(description)
+                print(*trailers_urls, sep="\n")
 
-        return [img, name, rating, year, description, trailers_urls]
+                return [img, name, rating, year, description, *trailers_urls]
 
-    else:
-        print(f"Ошибка {response.status_code}: {response.text}")
+            else:
+                print(f"Ошибка {resp.status}: {await resp.text()}")
+
+
+async def main():
+    await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
-    executor.start_polling(dp, skip_updates=True)
+    asyncio.run(main())
